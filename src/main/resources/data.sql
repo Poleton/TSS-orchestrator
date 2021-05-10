31e0851f5afec85f1735b4731daa7bff7128514f
iMofli <jordiperez10@icloud.com>
2021-05-10 17:40:56 +0200

    Extended sql code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-insert into user values (1, '0x9a6aaBD43f500c83df641c14C09B05076Cf281D0', '0x9a6aaBD43f500c83df641c14C09B05076Cf281D0', '0x9a6aaBD43f500c83df641c14C09B05076Cf281D0', 'user1', '1234', 'f1e48d4c2a7f36428f6d4c3b581d10973c36a7ad194e36de5e1b5c3d5c8cd789');
+insert into user (id,broker_address,client_address,insurance_address,name,email,password,private_key)
+values (1, '0x9a6aaBD43f500c83df641c14C09B05076Cf281D0','0x9a6aaBD43f500c83df641c14C09B05076Cf281D0', '0x9a6aaBD43f500c83df641c14C09B05076Cf281D0', 'user1','user1@tss.com', '1234','f1e48d4c2a7f36428f6d4c3b581d10973c36a7ad194e36de5e1b5c3d5c8cd789');
